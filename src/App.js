0df76a72963ab387a995ba752b0be0dbd57a37e7
Louis Gurung <louis.gurung@selu.edu>
2020-10-31 16:28:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./component/navbar";
+import About from "./component/about";
+import Footer from "./component/footer";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <About/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
