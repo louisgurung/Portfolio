8594a7bc18d232dc0f591499b815c304ab65cf9d
Louis Gurung <louis.gurung@selu.edu>
2021-01-21 09:21:39 -0600

    added better layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,138 @@
-import './App.css';
-import Navbar from "./component/navbar";
+import "./App.css";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import About from "./component/about";
 import Footer from "./component/footer";
+import Project from "./component/Project";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "80%",
+    maxWidth: "1100px",
+    margin: "0 auto",
+    background: "#fcf2e8",
+    backgroundRepeat: "repeat-x",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "#fcf2e8",
+    color: "black",
+    padding: "1em 0",
+    height: "auto",
+    margin: 0,
+    [theme.breakpoints.down("sm")]: {
+      height: "auto",
+    },
+  },
+  toolbar: {
+    height:"5vh",
+    display: "flex",
+    flexDirection: "row",
+    margin: 0,
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  navItems: {
+    width: "50%",
+    display: "flex",
+    flexShrink: 1,
+    flexGrow: 1,
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  nameTitle: {
+    width: "50%",
+    textAlign: "center",
+    fontFamily: "Satisfy",
+    fontSize: "2.5rem",
+    padding: "0.5em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.5rem",
+      width: "100%",
+    },
+  },
+  title: {
+    marginRight: "auto",
+    "&:hover, &:focus": {
+      cursor: "pointer",
+      color: "#F16A70",
+      textDecoration: "none",
+      backgroundImage: "linear-gradient(to right, #F16A70, #F16A70)",
+      backgroundPosition: "bottom",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "100% 2px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      margin: "0.15rem auto",
+      fontSize: "1rem",
+    },
+  },
+}));
+
+const App = (props) => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-     <Navbar/>
-     <About/>
-     <Footer/>
+    <div
+      className="container"
+      style={{
+        background: "#fcf2e8",
+      }}
+    >
+      <ElevationScroll {...props}>
+        <AppBar position="sticky" className={classes.appbar}>
+          <Toolbar className={classes.toolbar}>
+            <div className={classes.nameTitle}>LOUIS GURUNG</div>
+            <div className={classes.navItems}>
+              <Typography variant="h5" className={classes.title}>
+                AboutMe
+              </Typography>
+              <Typography variant="h5" className={classes.title}>
+                Skills
+              </Typography>
+              <Typography variant="h5" className={classes.title}>
+                Projects
+              </Typography>
+              <Typography variant="h5" className={classes.title}>
+                Contact
+              </Typography>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <About />
+      <Project />
+      <Footer />
     </div>
   );
+};
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
 }
 
 export default App;
--- src/component/about.js
+++ src/component/about.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Image from "../assets/Photo.jpg";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
+  container: {
+    width: "100%",
+    margin: 0,
+    padding: "50px 0",
+    display: "flex",
+  },
+  pic: {
+    width: "30em",
+    margin: "0 10vw",
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
+    "&:hover": {
+      elevation: "25",
+    },
+  },
+  about: {
+    width: "60%",
+    maxWidth: "800px",
+    margin: "5vh 10vw",
+    fontWeight: "bold",
+    fontSize: "1.125rem",
+    fontFamily: "Nunito",
+    background:
+      "linear-gradient(90deg, rgba(238,237,193,1) 0%, rgba(239,210,244,1) 100%) !important",
+    padding: "2em",
+    textAlign: "center",
   },
 }));
 
@@ -18,18 +42,30 @@ export default function CenteredGrid() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>
-              Image here
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper className={classes.paper}>About me</Paper>
-        </Grid>
-        
-      </Grid>
+    <div className={classes.container}>
+      {/* <img
+        src={Image}
+        class="animate__animated animate__pulse"
+        className={classes.pic}
+        alt="HeaderImage"
+        // style={{
+        //   width: "50vw",
+        //   height: "40vh",
+        // }}
+      /> */}
+      <Paper elevation={5} className={classes.about}>
+        <h1>About me</h1>
+        <h4 class="animate__animated animate__backInRight">
+          I am Louis Gurung.I am a Full Stack Web/Mobile Application Developer.
+          I graduated from Southeastern Louisiana University with Bachelors
+          degree in Computer Science and minor in Mathematics. I love turning
+          ideas to real world applications. I love working with people who are
+          motivated and help each other grow. Besides work, I love cooking,
+          travelling and being around good company. My motto in life is "Stay
+          Happy".
+        </h4>
+      </Paper>
+      {/* <Paper className={classes.paper}>About me</Paper> */}
     </div>
   );
 }
